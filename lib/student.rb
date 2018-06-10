b8330230f04486c680f5b252327a47aeb55fb5c4
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-06-10 04:38:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,7 @@
 require_relative './user.rb'
 class Student < User
 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
+  def initialize
     @knowledge = []
   end
 
